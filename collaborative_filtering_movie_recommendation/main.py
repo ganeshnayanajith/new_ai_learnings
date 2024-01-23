65034fe1bc29069a0d656c7b9b948bf4f65eb3d5
Ganesh Nayanajith <32520964+ganeshnayanajith@users.noreply.github.com>
2024-01-23 14:05:44 +0530

    0123

--- collaborative_filtering_movie_recommendation/main.py
+++ collaborative_filtering_movie_recommendation/main.py
@@ -41,3 +41,66 @@ print(ratings.head())
 print("\nItem Data :")
 print("shape : ", items.shape)
 print(items.head())
+
+print('-----------------------------------------------')
+
+r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
+ratings_train = pd.read_csv('ml-100k/ua.base', sep='\t', names=r_cols, encoding='latin-1')
+ratings_test = pd.read_csv('ml-100k/ua.test', sep='\t', names=r_cols, encoding='latin-1')
+print(ratings_train.shape)
+print(ratings_test.shape)
+
+print('-----------------------------------------------')
+
+n_users = ratings.user_id.unique().shape[0]
+n_items = ratings.movie_id.unique().shape[0]
+
+print(n_users)
+print(n_items)
+
+print('-----------------------------------------------')
+
+data_matrix = np.zeros((n_users, n_items))
+print(data_matrix)
+for line in ratings.itertuples():
+    print(line)
+    print(line[1])
+    print(line[1] - 1)
+    data_matrix[line[1] - 1, line[2] - 1] = line[3]
+
+print(data_matrix)
+
+print('-----------------------------------------------')
+
+from sklearn.metrics.pairwise import pairwise_distances
+
+user_similarity = pairwise_distances(data_matrix, metric='cosine')
+item_similarity = pairwise_distances(data_matrix.T, metric='cosine')
+
+print(user_similarity)
+print(item_similarity)
+
+print('-----------------------------------------------')
+
+
+def predict(ratings, similarity, type='user'):
+    if type == 'user':
+        mean_user_rating = ratings.mean(axis=1)
+        # We use np.newaxis so that mean_user_rating has same format as ratings
+        ratings_diff = (ratings - mean_user_rating[:, np.newaxis])
+        pred = mean_user_rating[:, np.newaxis] + similarity.dot(ratings_diff) / np.array(
+            [np.abs(similarity).sum(axis=1)]).T
+    elif type == 'item':
+        pred = ratings.dot(similarity) / np.array([np.abs(similarity).sum(axis=1)])
+    return pred
+
+
+user_prediction = predict(data_matrix, user_similarity, type='user')
+item_prediction = predict(data_matrix, item_similarity, type='item')
+
+print('user_prediction')
+print(user_prediction)
+print('item_prediction')
+print(item_prediction)
+
+print('-----------------------------------------------')
