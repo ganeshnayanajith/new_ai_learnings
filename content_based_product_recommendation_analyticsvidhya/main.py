10214d5082f2cc289fbbc6cd218a2550d483a110
Ganesh Nayanajith <32520964+ganeshnayanajith@users.noreply.github.com>
2024-01-29 16:25:19 +0530

    0129

--- content_based_product_recommendation_analyticsvidhya/main.py
+++ content_based_product_recommendation_analyticsvidhya/main.py
@@ -20,3 +20,32 @@ import pandas as pd
 df = pd.read_csv('BigBasket Products.csv', index_col='index')
 print(df.shape)
 print(df.head())
+
+print('Null Data Count In Each Column')
+print('-' * 30)
+print(df.isnull().sum())
+
+print('-' * 30)
+print('Null Data % In Each Column')
+print('-' * 30)
+
+# returns the number of rows in the DataFrame
+print(f'df.shape[0] : {df.shape[0]}')
+
+print('-' * 30)
+
+for col in df.columns:
+    null_count = df[col].isnull().sum()
+    total_count = df.shape[0]
+    print("{} : {:.2f}".format(col, null_count / total_count * 100))
+
+print('-' * 30)
+
+df = df.dropna()
+print(df.shape)
+
+print('-' * 30)
+
+print(df.dtypes)
+
+print('-' * 30)
