c6e562d10f5364b4596848176d077090b031c645
Ganesh Nayanajith <32520964+ganeshnayanajith@users.noreply.github.com>
2024-01-29 14:41:24 +0530

    Create main.py

---
+++ content_based_product_recommendation_analyticsvidhya/main.py
@@ -0,0 +1,16 @@
+# Basic Libraries
+
+import numpy as np
+import pandas as pd
+
+# Visualization Libraries
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+# Text Handling Libraries
+
+import re
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
